27ff2bbeacd3221f29fd05e0ccd70704a5a1ffa1
boarderbare <boarderbare@gmail.com>
2022-06-14 13:43:32 +0400

    initial

---
+++ src/main/java/ru/netology/repository/Repository.java
@@ -0,0 +1,44 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Product;
+import ru.netology.repository.NotFoundException;
+
+public class Repository {
+    private Product[] items = new Product[0];
+
+    public void save(Product item) {
+        Product[] tmp = new Product[items.length + 1];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        tmp[tmp.length - 1] = item;
+        items = tmp;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
+
+    public Product findById(int id) {
+        for (Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+
+    }
+
+    public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException("Element with id: " + id + " not found");
+        }
+        Product[] tmp = new Product[items.length - 1];
+        int index = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        items = tmp;
+    }
+}
---
+++ src/test/java/ru/netology/domain/ProductRepositoryTest.java
@@ -0,0 +1,40 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.repository.NotFoundException;
+import ru.netology.repository.Repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductRepositoryTest {
+    private Repository repository = new Repository();
+    private Product first = new Product(1, "Motocycle", 9000);
+    private Product second = new Product(2, "Photocamera", 1200);
+    private Product third = new Book(3, "War and Peace", 34, "Leo Tolstoy");
+    private Product fourth = new Smartphone(4, "iPhone", 1500, "Apple");
+
+    @BeforeEach
+    public void setUp() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        repository.removeById(2);
+        Product[] actual = repository.findAll();
+        Product[] expected = new Product[]{first, third, fourth};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemoveByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(6);
+        });
+    }
+}
